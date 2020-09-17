16543de185cb70edda933a166c398947d5cfe8bf
Petar Mihaylovski <459725@student.saxion.nl>
2020-09-17 10:58:17 +0200

    first person camera and movement ready

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController characterController;
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public float jumpHeight = 3f;
+    
+    private Vector3 velocity;
+    private bool onTheGround;
+    
+    // Update is called once per frame
+    void Update() {
+        onTheGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (onTheGround && velocity.y < 0f) {
+            velocity.y = -2f;
+        }
+        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //player movement
+        Vector3 movement = transform.forward * z + transform.right * x;
+        characterController.Move(movement * speed * Time.deltaTime);
+
+        if (Input.GetButton("Jump") && onTheGround) {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f  * gravity);
+        }
+        //player gravity
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+}
